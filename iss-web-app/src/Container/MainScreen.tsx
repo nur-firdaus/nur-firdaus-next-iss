11ed7302eafbec38a4111e0147a2cc07b085b0c4
firdausjob <nurfirdaus.job@gmail.com>
2021-11-30 23:13:47 +0800

    table for data

--- iss-web-app/src/Container/MainScreen.tsx
+++ iss-web-app/src/Container/MainScreen.tsx
@@ -1,4 +1,4 @@
-import { Badge, Breadcrumb, Button, Card, Col, ConfigProvider, DatePicker, FormInstance, Layout, Menu, Row, Space, Spin, Tabs } from "antd"
+import { Badge, Breadcrumb, Button, Card, Col, ConfigProvider, DatePicker, FormInstance, Layout, Menu, Row, Space, Spin, Table, Tabs } from "antd"
 
 import axios from "axios"
 import React, { useState, useEffect, useContext } from "react"
@@ -12,9 +12,6 @@ import { TSatellite, TSatelliteDetails } from "../Types/Setellites"
 import { UserOutlined, LaptopOutlined, NotificationOutlined } from "@ant-design/icons"
 import JSONPretty from "react-json-pretty"
 import moment from "moment"
-
-const { SubMenu } = Menu
-const { Header, Content, Footer, Sider } = Layout
 
 type TProps = {}
 
@@ -36,6 +33,8 @@ const MainScreen: React.FC<TProps> = (props) => {
         solar_lon: 0,
         units: "",
     })
+
+    const [satellitesDetails, setSatellitesDetails] = useState<TSatelliteDetails[]>([])
 
     useEffect(() => {
         const fetchData = async () => {
@@ -68,18 +67,31 @@ const MainScreen: React.FC<TProps> = (props) => {
     }, [listOfsatellites])
 
     function onGetDateValue(value: any) {
-        const ts = moment(value).unix()
+        const valueTimeStamp = moment(value).unix()
         const mDate = moment.utc(value).local()
         console.log("onOk: ", value, mDate)
-        console.log("ts: ", ts)
+        console.log("ts: ", valueTimeStamp)
+        //1 min = 6000 timestamp
+        //6000*50min=300000
+        const earlyTimeStamp = valueTimeStamp - 6000 * 50
 
-        fetchLocationBasedTimeZone(ts)
+        processTimeZone(earlyTimeStamp)
     }
-    const fetchLocationBasedTimeZone = async (ts: number) => {
+
+    const processTimeZone = async (ts: number) => {
+        let text = ""
+        for (let i = 0; i < 10; i++) {
+            text += ts + ","
+        }
+        console.log(text)
+        fetchLocationBasedTimeZone(text)
+    }
+
+    const fetchLocationBasedTimeZone = async (ts: string) => {
         setLoading(true)
         try {
             const { data: response } = await axios.get(`https://api.wheretheiss.at/v1/satellites/${listOfsatellites[0].id}/positions?timestamps=${ts}&units=miles`)
-            console.log("TS", response)
+            setSatellitesDetails(response)
         } catch (error: any) {
             console.error(error.message)
         }
@@ -91,33 +103,103 @@ const MainScreen: React.FC<TProps> = (props) => {
 
     const { Header, Content, Footer } = Layout
     const { TabPane } = Tabs
+
+    const columns: any = [
+        {
+            title: "latitude",
+            dataIndex: "latitude",
+            key: "latitude",
+            render: (value: any, record: TSatelliteDetails) => <>{record.latitude}</>,
+        },
+        {
+            title: "longitude",
+            dataIndex: "longitude",
+            key: "longitude",
+            render: (value: any, record: TSatelliteDetails) => <>{record.longitude}</>,
+        },
+        {
+            title: "altitude",
+            dataIndex: "altitude",
+            key: "altitude",
+            render: (value: any, record: TSatelliteDetails) => <>{record.altitude}</>,
+        },
+        {
+            title: "velocity",
+            dataIndex: "velocity",
+            key: "velocity",
+            render: (value: any, record: TSatelliteDetails) => <>{record.velocity}</>,
+        },
+        {
+            title: "visibility",
+            dataIndex: "visibility",
+            key: "visibility",
+            render: (value: any, record: TSatelliteDetails) => <>{record.visibility}</>,
+        },
+        {
+            title: "footprint",
+            dataIndex: "footprint",
+            key: "footprint",
+            render: (value: any, record: TSatelliteDetails) => <>{record.footprint}</>,
+        },
+        {
+            title: "timestamp",
+            dataIndex: "timestamp",
+            key: "timestamp",
+            render: (value: any, record: TSatelliteDetails) => <>{record.timestamp}</>,
+        },
+        {
+            title: "daynum",
+            dataIndex: "daynum",
+            key: "daynum",
+            render: (value: any, record: TSatelliteDetails) => <>{record.daynum}</>,
+        },
+        {
+            title: "solar_lat",
+            dataIndex: "solar_lat",
+            key: "solar_lat",
+            render: (value: any, record: TSatelliteDetails) => <>{record.solar_lat}</>,
+        },
+        {
+            title: "solar_lon",
+            dataIndex: "solar_lon",
+            key: "solar_lon",
+            render: (value: any, record: TSatelliteDetails) => <>{record.solar_lon}</>,
+        },
+        {
+            title: "units",
+            dataIndex: "units",
+            key: "units",
+            render: (value: any, record: TSatelliteDetails) => <>{record.units}</>,
+        },
+    ]
+
     return (
         <>
-            <Layout className="layout">
-                <Header>
-                    <div className="logo" />
-                    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
-                        {new Array(15).fill(null).map((_, index) => {
-                            const key = index + 1
-                            return <Menu.Item key={key}>{`nav ${key}`}</Menu.Item>
-                        })}
-                    </Menu>
-                </Header>
-                <Content style={{ padding: "0 50px" }}>
-                    <Breadcrumb style={{ margin: "16px 0" }}>
-                        <Breadcrumb.Item>Home</Breadcrumb.Item>
-                    </Breadcrumb>
-                    <div className="site-layout-content">
-                        <Card title="ISS LOCATION">
-                            <JSONPretty id="json-pretty" data={satelliteDetails}></JSONPretty>
-                        </Card>
-                        <Card title="Getting the location of the ISS at a specific tim">
-                            <DatePicker onOk={onGetDateValue} bordered={true} showTime={true} suffixIcon="" format={"DD-MM-Y HH:mm:ss"} />
-                        </Card>
-                    </div>
-                </Content>
-                <Footer style={{ textAlign: "center" }}>Nur Firdaus ©2021 Created Using React.JS</Footer>
-            </Layout>
+            <Spin spinning={loading}>
+                <Layout className="layout">
+                    <Header>
+                        <div className="logo" />
+                        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}></Menu>
+                    </Header>
+                    <Content style={{ padding: "0 50px" }}>
+                        <Breadcrumb style={{ margin: "16px 0" }}>
+                            <Breadcrumb.Item>Home</Breadcrumb.Item>
+                        </Breadcrumb>
+                        <div className="site-layout-content">
+                            <Card title="ISS LOCATION">
+                                <JSONPretty id="json-pretty" data={satelliteDetails}></JSONPretty>
+                            </Card>
+                            <Card title="Getting the location of the ISS at a specific time">
+                                <DatePicker onOk={onGetDateValue} bordered={true} showTime={true} suffixIcon="" format={"DD-MM-Y HH:mm:ss"} />
+                            </Card>
+                            <Card title="Result">
+                                <Table scroll={{ x: 1500, y: 300 }} columns={columns} dataSource={satellitesDetails} />,
+                            </Card>
+                        </div>
+                    </Content>
+                    <Footer style={{ textAlign: "center" }}>Nur Firdaus ©2021 Created Using React.JS</Footer>
+                </Layout>
+            </Spin>
         </>
     )
 }
